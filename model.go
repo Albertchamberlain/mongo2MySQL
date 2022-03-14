523d0932ccda13ac24bb5f72d049d20472c9fa1f
Amos <47343901+Albertchamberlain@users.noreply.github.com>
2022-03-14 23:34:53 +0800

    <add>:<model.go>

---
+++ model.go
@@ -0,0 +1,29 @@
+package model
+
+type Community struct {
+	ID int
+	Province string
+	City string
+	District string
+	Name string
+	Url string
+	DetailAddress string
+	Coord string
+	Price string
+	PropertyType string
+	PropertyFee string
+	Area string
+	HouseCount string
+	CompletionTime string
+	ParkingCount string
+	PlotRatio string
+	GreeningRate string
+	PropertyCompany string
+	Developers string
+	DistractedMachong string
+	ReturnWejd string
+	ReturnTejd string
+	ReportTime string
+	GlobalDist string
+
+}
